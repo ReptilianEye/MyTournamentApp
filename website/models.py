a64231eae584f735bc72c7df1db1fa65ba51aa68
ReptilianEye <piotr.rzadkowski@outlook.com>
2021-09-21 21:36:56 +0200

    update admin to is_admin

--- website/models.py
+++ website/models.py
@@ -14,7 +14,7 @@ class User(db.Model,UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
 
-    admin = db.Column(db.Boolean,default=False) #TODO zmienic na isAdmin - poniewaz boolean
+    is_admin = db.Column(db.Boolean,default=False) 
 
     players = db.relationship('Player')  
 
