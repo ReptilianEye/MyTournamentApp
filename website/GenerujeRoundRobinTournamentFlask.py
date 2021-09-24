199dd1014d1618fd373230c018d235114f17a930
DeserekMalinowy <marek40.kenaj9@gmail.com>
2021-09-24 16:12:16 +0200

    Update GenerujeRoundRobinTournamentFlask.py

--- website/GenerujeRoundRobinTournamentFlask.py
+++ website/GenerujeRoundRobinTournamentFlask.py
@@ -168,4 +168,28 @@ def WygenerujTermiarz(Uczestnicy):
     Terminarz = PrzygotowujeTerminarzDoWyswietlenia(Terminarz)
     return Terminarz
 
+def DzieliNa2Teamy(ListaZawodników):
+    lista1=[]
+    lista2=[]
+
+    # i=int(input("ilośczadadasfdadwd"))
+    # for j in range (i):
+    #     imie=input("imoona ")
+    #     lista.append(imie)
+
+    random.shuffle(ListaZawodników)
+    n=1
+    for imie in ListaZawodników:
+        if n==1:
+            lista1.append(imie)
+        else:
+            lista2.append(imie)
+        n*=-1
+    Treminarz=[]
+    Treminarz.append(lista1)
+    Treminarz.append(lista2)
+    print(Treminarz)
+
+DzieliNa2Teamy(["Tomek", "Kamil", "piot","roit"])
+
 
