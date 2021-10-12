017eff7274b39dc04b7d44e4a820d89a7ff7e5ee
Klaudiusz16 <klaudiusz.kurbiel55@gmail.com>
2021-10-12 17:40:37 +0200

    .

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = create_app()
 # zabezpiecza to przypadek w ktorym bym importowal 'main.py' a ta by sie chciala wystartowac
 if __name__ == '__main__':
     # debug = true - za kazdym razem kiedy wprowadze zmiane w kodzie serwer sie zresetuje i pokaze zmiany
-    app.run(debug=True, host="192.168.12.53") 
-    # Serwer startuje w lokalnej sieci, udostępnia możliwość uruchomienia strony na telefonach i innych urządeniach w tej samej sieci. (IP for change)
+    app.run(debug=True, host="0.0.0.0") 
+    # Serwer startuje w lokalnej sieci, udostępnia możliwość uruchomienia strony na telefonach i innych urządeniach w tej samej sieci. ()
