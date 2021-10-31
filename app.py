5f7fc33270cb34198ae811e8bf4653055795b9d3
ReptilianEye <piotr.rzadkowski@outlook.com>
2021-10-31 10:21:01 +0100

    Last visual changes

--- app.py
+++ app.py
@@ -7,4 +7,3 @@ app = create_app()
 if __name__ == '__main__':
     # debug = true - za kazdym razem kiedy wprowadze zmiane w kodzie serwer sie zresetuje i pokaze zmiany
     app.run(debug=True)
-    # testowanie branchu
