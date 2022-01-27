c72233c309b727f55f8889b53bd165fdc0695cab
beznicku <pablo_tpl@interia.pl>
2022-01-27 18:39:20 +0100

    Duzo zmian

--- website/static/index.js
+++ website/static/index.js
@@ -76,3 +76,18 @@ function deleteOpponent(opponentId) {
   });
 }
 
+// PasswordFunction SHOW/HIDE
+
+function PasswordFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+      x.type = "text";
+      document.getElementById('hide').style.display = "inline-block";
+      document.getElementById('show').style.display = "none";
+  }
+  else {
+      x.type = "password";
+      document.getElementById('hide').style.display = "none";
+      document.getElementById('show').style.display = "inline-block";
+  }
+}
